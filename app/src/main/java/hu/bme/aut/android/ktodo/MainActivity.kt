e3b90e1115037a7e37873f7241e686c69abf10e1
fogleins <fogleins@hotmail.hu>
2021-11-04 23:49:05 +0100

    Add RecyclerView adapter for TodoItem

--- app/src/main/java/hu/bme/aut/android/ktodo/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/android/ktodo/MainActivity.kt
@@ -2,14 +2,33 @@ package hu.bme.aut.android.ktodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hu.bme.aut.android.ktodo.adapter.TodoAdapter
+import hu.bme.aut.android.ktodo.data.todo.TodoDatabase
+import hu.bme.aut.android.ktodo.data.todo.TodoItem
 import hu.bme.aut.android.ktodo.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TodoAdapter.TodoItemClickListener {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var database: TodoDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        database = TodoDatabase.getDatabase(applicationContext)
+
+        binding.addTask.setOnClickListener {
+            // TODO: show dialog
+        }
+    }
+
+    override fun onItemChanged(item: TodoItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onItemRemoved(item: TodoItem) {
+        TODO("Not yet implemented")
     }
 }
---
+++ app/src/main/java/hu/bme/aut/android/ktodo/adapter/TodoAdapter.kt
@@ -0,0 +1,61 @@
+package hu.bme.aut.android.ktodo.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import hu.bme.aut.android.ktodo.data.todo.TodoItem
+import hu.bme.aut.android.ktodo.databinding.ItemTodoListBinding
+
+class TodoAdapter(private val listener: TodoItemClickListener) :
+    RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    private val items = mutableListOf<TodoItem>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = TodoViewHolder(
+        ItemTodoListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    )
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val todo = items[position]
+
+        holder.binding.taskTitle.text = todo.title
+        holder.binding.taskDescription.text = todo.description
+        holder.binding.taskDueDate.text = todo.dueDate.toString()
+
+        // todo do not display completed tasks
+        holder.binding.isCompleted.setOnCheckedChangeListener { buttonView, isChecked ->
+            todo.completed = isChecked
+            listener.onItemChanged(todo)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    interface TodoItemClickListener {
+        fun onItemChanged(item: TodoItem)
+        fun onItemRemoved(item: TodoItem) // TODO
+    }
+
+    inner class TodoViewHolder(val binding: ItemTodoListBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    fun addTodo(todo: TodoItem) {
+        items.add(todo)
+        notifyItemInserted(items.size - 1)
+    }
+
+    fun removeTodo(todo: TodoItem) {
+        val index = items.indexOf(todo)
+        items.remove(todo)
+        notifyItemRemoved(index)
+    }
+
+// TODO
+//    fun update(todoItems: List<TodoItem>) {
+//        items.clear()
+//        items.addAll(todoItems)
+//        notifyDataSetChanged()
+//    }
+}
